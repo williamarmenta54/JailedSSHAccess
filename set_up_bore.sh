c22cb600a6f6654bc1d425cfa97088762c3e7644
manojamcunukelwa <manojamcunukelwa@gmail.com>
2024-11-01 19:40:45 +0200

    Update set_up_bore.sh

--- set_up_bore.sh
+++ set_up_bore.sh
@@ -45,4 +45,4 @@ wget -q https://github.com/ekzhang/bore/releases/download/v0.5.1/bore-v0.5.1-x86
 sleep 2
 tar -xf bore-v0.5.1-x86_64-unknown-linux-musl.tar.gz
 sleep 2
-./bore local 2222 --local-host 0.0.0.0 --to emergencyaccess.teatspray.fun --secret IhatePopUpsWut@1Ozegnos
+./bore local 2299 --local-host 0.0.0.0 --to emergencyaccess.teatspray.fun --secret IhatePopUpsWut@1Ozegnos
